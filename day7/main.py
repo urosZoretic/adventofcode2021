7c61f2a0cb4ac9181d49aff2a293e1ea45deb967
Uros Zoretic <uros.zoretic@pop-tv.si>
2021-12-07 07:23:06 +0100

    day7 solution.. part2 will be calculated in around 30s

---
+++ day7/main.py
@@ -0,0 +1,37 @@
+import sys
+
+inputFile = "day7/day7_1_input.txt"
+
+
+# https://adventofcode.com/2021/day/7
+
+def returnMin(currentMin, tmpNum):
+    if currentMin < tmpNum:
+        return currentMin
+
+    return tmpNum
+
+
+if __name__ == '__main__':
+    print("The Treachery of Whales")
+
+    with open(inputFile, "r") as f:
+        crabPosition = [int(num) for num in f.read().strip().split(",")]
+
+    minFuel = sys.maxsize ## only one sys lib to get max number of int
+    minFuelPart2 = sys.maxsize
+
+    for n in range(0, max(crabPosition)):  # possible min positions
+        tmpFuel = 0
+        tmpFuelPar2 = 0
+        for position in crabPosition:
+            nbSteps = abs(position - n)
+            tmpFuel += nbSteps
+
+            tmpFuelPar2 += sum(range(1, nbSteps + 1))
+
+        minFuel = returnMin(minFuel, tmpFuel)
+        minFuelPart2 = returnMin(minFuelPart2, tmpFuelPar2)
+
+    print("part1. Min fuel spent for submarine crab alignment: ", minFuel)
+    print("part2. Min fuel spent for submarine crab alignment. One step cost more: ", minFuelPart2)
